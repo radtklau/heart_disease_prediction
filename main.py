1be3263e0b21b0b0189958a4ae6408ac44d1d9fa
Laurids Radtke <laurids.radtke1@gmail.com>
2025-04-05 17:57:57 +0200

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+heart_disease = fetch_ucirepo(id=45) 
+  
+# data (as pandas dataframes) 
+X = heart_disease.data.features 
+y = heart_disease.data.targets
+  
+# metadata 
+print(heart_disease.metadata)
+  
+# variable information 
+print(heart_disease.variables)
+
+# import os
+# print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+# print("PATH:", os.environ.get('PATH'))
